c218dfecbf617054651d50d9a495c582e3c020ca
zuoleili <73096759+stoneli1117@users.noreply.github.com>
2021-11-09 22:02:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        Button trigger = (Button) findViewById(R.id.trigger);
+        trigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
